9c372d300e937391e55eb5ca0b425f6b1f36b401
benyankovich <ben.yankovich@gmail.com>
2021-06-23 21:46:48 +0300

    client auth

--- client/src/App.js
+++ client/src/App.js
@@ -1,32 +1,45 @@
-import React, {Fragment, ButtonHTMLAttributes} from "react";
-import './App.css';
-import Navbar from "./components/layout/Navbar";
-import Landing from "./components/layout/Landing";
-import Alert from "./components/layout/alert";
-import Login from "./components/auth/Login";
-import Register from "./components/auth/Register";
-import {BrowserRouter as Router, Route, Switch} from "react-router-dom";
+import React, { Fragment, useEffect } from 'react';
+import { BrowserRouter as Router, Route, Switch } from 'react-router-dom';
+import Navbar from './components/layout/Navbar';
+import Landing from './components/layout/Landing';
+import Routes from './components/routing/Routes';
+import { LOGOUT } from './actions/types';
+
 // Redux
-import {Provider} from "react-redux";
-import store from "./store";
+import { Provider } from 'react-redux';
+import store from './store';
+import { loadUser } from './actions/auth';
+import setAuthToken from './utils/setAuthToken';
 
-const App = () => (
-    <Provider store={store}>
-        <Router>
-            <Fragment>
-                <Navbar />
-                <Route exact path="/" component={Landing} />
-                <section className="container">
-                    <Alert />
-                        <Switch>
-                            <Route exact path="/register" component={Register} />
-                            <Route exact path="/login" component={Login} />
-                        </Switch>
-                </section>
-            </Fragment>
-        </Router>
-    </Provider>
-)
+import './App.css';
 
+const App = () => {
+    useEffect(() => {
+        // check for token in LS
+        if (localStorage.token) {
+            setAuthToken(localStorage.token);
+        }
+        store.dispatch(loadUser());
+
+        // log user out from all tabs if they log out in one tab
+        window.addEventListener('storage', () => {
+            if (!localStorage.token) store.dispatch({ type: LOGOUT });
+        });
+    }, []);
+
+    return (
+        <Provider store={store}>
+            <Router>
+                <Fragment>
+                    <Navbar />
+                    <Switch>
+                        <Route exact path="/" component={Landing} />
+                        <Route component={Routes} />
+                    </Switch>
+                </Fragment>
+            </Router>
+        </Provider>
+    );
+};
 
 export default App;
